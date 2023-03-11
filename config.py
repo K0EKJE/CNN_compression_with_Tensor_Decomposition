73f872d34d3b6bbbf437314a24f26e06fc9f5a95
Haoran RAIN <95905185+K0EKJE@users.noreply.github.com>
2023-03-11 15:30:57 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 
 from models import *
-model = AlexNet()
+model = VGG('VGG19')
 # Params for training
 learning_rate = 0.001
 momentum = 0.9
 
-# new_weight_path = "/content/CNN_compression_with_Tensor_Decomposition/trained_weights/VGG19_240iter_ckpt.pth"
-new_weight_path = "/content/checkpoint/ckpt.pth" 
-layer_to_decomp = [3] # list determining layers to decompose
+new_weight_path = "/content/CNN_compression_with_Tensor_Decomposition/trained_weights/VGG19_240iter_ckpt.pth"
+# new_weight_path = "/content/checkpoint/ckpt.pth" 
+layer_to_decomp = [0] # list determining layers to decompose
 
 # rank chosen from 'auto' , "full" or desired number
-rank = "full"
+rank = [3]
 # whether to add residual structure to the decomposed model; oroginally designed to account for 
 # possible gradient problem, but proven to be not successful.
 res = False
