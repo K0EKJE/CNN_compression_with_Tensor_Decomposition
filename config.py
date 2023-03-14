e4bc21bfada31c685cb16d7597be39dbebc1df19
Haoran RAIN <95905185+K0EKJE@users.noreply.github.com>
2023-03-14 16:26:49 -0700

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,18 @@ momentum = 0.9
 
 new_weight_path = "/content/CNN_compression_with_Tensor_Decomposition/trained_weights/VGG19_240iter_ckpt.pth"
 # new_weight_path = "/content/checkpoint/ckpt.pth" 
-layer_to_decomp = [0] # list determining layers to decompose
+layer_to_decomp = [7] # list determining layers to decompose
 
-# rank chosen from 'auto' , "full" or desired number
+# rank chosen from 'auto' , "full" or desired number for CP
 rank = [3]
+
+# rank selection for Tucker, QR VBMF, SVD
+tucker_rank_selection_method = 'SVD'
+
 # whether to add residual structure to the decomposed model; oroginally designed to account for 
 # possible gradient problem, but proven to be not successful.
 res = False
-fine_tune_epochs = 15 # 
+fine_tune_epochs = 10 # 
 
 # used pretrained model to test accuracy
 model_path = "/content/decomposed_model"
